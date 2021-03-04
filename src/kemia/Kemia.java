68c16d73a2ebf840f126e71dcc9fc1e1c4ce1762
Komonyi <komonyi1996@gmail.com>
2021-03-04 11:21:21 +0100

    3.Feladat 8.Feladat

--- src/kemia/Kemia.java
+++ src/kemia/Kemia.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Kemia {
 
@@ -21,6 +23,32 @@ public class Kemia {
             }
             i++;
         }
+        System.out.println("Elemek száma: " + elemek.size());
+        
+        
+         HashMap<String, Integer> hs = new HashMap<>();
+         
+        for (Elem elem : elemek) {
+           String kulcs = elem.getEv();
+            if (!hs.containsKey(kulcs)) {
+                hs.put(kulcs, 1);
+            } else {
+                int szam = hs.get(kulcs);
+                hs.put(kulcs, szam + 1);
+            }
+            
+            
+        }
+           for (Map.Entry<String, Integer> entry : hs.entrySet()) {
+                String key = entry.getKey();
+                
+                Integer value = entry.getValue();
+                if (value>=3) {
+                     System.out.printf("Év: %s szám: %d \n", key,value);
+               }
+                 
+            }
     }
+   
     
 }
