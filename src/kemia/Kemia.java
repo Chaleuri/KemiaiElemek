edd61a0112519b472a814ce434e331269083aa59
Peti <peter.hathazi99@gmail.com>
2021-03-04 10:29:35 +0100

    2. feladat

--- src/kemia/Kemia.java
+++ src/kemia/Kemia.java
@@ -1,8 +1,15 @@
 package kemia;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Kemia {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
+        List<String> beolvasott = Files.readAllLines(Paths.get("felfedezesek.csv"));
         
     }
     
