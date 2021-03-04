4258fe7e88198407566663924467e0fc988b86ff
Peti <peter.hathazi99@gmail.com>
2021-03-04 10:55:03 +0100

    2. feladat (javítva)

---
+++ src/kemia/Elem.java
@@ -0,0 +1,36 @@
+package kemia;
+
+public class Elem {
+    
+    private String ev, elem, vegyjel,felfedezo;
+    private int rendszam;
+
+    public Elem(String ev, String elem, String vegyjel, String rendszam, String felfedezo) {
+        this.ev = ev;
+        this.elem = elem;
+        this.vegyjel = vegyjel;
+        this.felfedezo = felfedezo;
+        this.rendszam = Integer.parseInt(rendszam);
+    }
+
+    public String getEv() {
+        return ev;
+    }
+
+    public String getElem() {
+        return elem;
+    }
+
+    public String getVegyjel() {
+        return vegyjel;
+    }
+
+    public String getFelfedezo() {
+        return felfedezo;
+    }
+
+    public int getRendszam() {
+        return rendszam;
+    }
+    
+}
--- src/kemia/Kemia.java
+++ src/kemia/Kemia.java
@@ -3,14 +3,24 @@ package kemia;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Kemia {
 
     public static void main(String[] args) throws IOException {
         
-        List<String> beolvasott = Files.readAllLines(Paths.get("felfedezesek.csv"));
-        
+        List<String> sorok = Files.readAllLines(Paths.get("felfedezesek.csv"));
+        ArrayList<Elem> elemek = new ArrayList<>();
+        int i = 0;
+        for (String sor : sorok) {
+            
+            String[] adatok = sor.split(";");
+            if (i>0){
+                elemek.add(new Elem(adatok[0], adatok[1], adatok[2], adatok[3], adatok[4]));
+            }
+            i++;
+        }
     }
     
 }
